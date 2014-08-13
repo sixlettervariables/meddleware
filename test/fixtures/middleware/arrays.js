61f25368b079420fdaf75615cc9587dddcb28bb7
Christopher <christopher.watford@gmail.com>
2014-08-13 10:24:32 -0400

    Adding dummy data to for array test fixture

--- test/fixtures/middleware/arrays.js
+++ test/fixtures/middleware/arrays.js
@@ -3,12 +3,14 @@
 module.exports = function (config) {
     var middleware = [];
     
-    middleware.push(function ArrayFirst(req, res) {
-    
+    middleware.push(function ArrayFirst(req, res, next) {
+        res.locals.arrayFirst = true;
+        next();
     });
     
-    middleware.push(function ArraySecond(req, res) {
-    
+    middleware.push(function ArraySecond(req, res, next) {
+        if (res.locals.arrayFirst) res.locals.arraySecond = true;
+        next();
     });
     
     return middleware;
