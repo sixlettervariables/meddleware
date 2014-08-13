345b49037d61629c772c51820167779c84a3e5bb
Christopher <christopher.watford@gmail.com>
2014-08-13 10:22:42 -0400

    Test fixture for middleware defined by array

---
+++ test/fixtures/middleware/arrays.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function (config) {
+    var middleware = [];
+    
+    middleware.push(function ArrayFirst(req, res) {
+    
+    });
+    
+    middleware.push(function ArraySecond(req, res) {
+    
+    });
+    
+    return middleware;
+};
